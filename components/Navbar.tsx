d0c68ea1b7ae2ddcdd33d31f697f488d0fd27a31
MySagarGithub <Pandaysagar2004@gmail.com>
2025-09-05 22:55:15 +0545

    added nav bar

---
+++ components/Navbar.tsx
@@ -0,0 +1,58 @@
+"use client"
+import React from 'react'
+import Logo from "@/components/logo";
+import { usePathname } from 'next/navigation';
+function Navbar() {
+  return (
+  <>   
+  <DesktopNavbar />
+  </>
+
+  );
+  
+}
+
+const items = [
+    {
+        label: "Dashboard", link:"/" },
+    { label: "Transactions", link:"/transactions" },
+    { label: "Manage", link:"/manage" },
+    
+];
+
+function DesktopNavbar() {
+    return (
+        <div className="hidden-separate border-b
+        bg-background md:block">
+           <nav className="container flex items-center
+           justify-between px-8">
+            <div className='flex h-[80px] min-h-[60px] '>
+                <Logo />
+                <div className='flex h-full'>
+                    {items.map((item) => (<NavbarItem 
+                    key={item.label}
+                    link={item.link}
+                    label={item.label} />
+                ))}
+                </div>
+            </div>
+           </nav>
+        </div>
+    )
+}
+function NavbarItem({label, link}: 
+    {
+    label: string;
+    link: string;
+}){
+    const pathname = usePathname();
+    const isActive = pathname === link;
+    return (
+    <div className='relativec flex items-center'>
+        <link href={link}>{label}</link> 
+    </div>
+    )
+    }
+
+
+export default Navbar;
